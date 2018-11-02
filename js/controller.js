80a329822b40b7ff8a4d7a0bbf43a8fb8905da9a
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-11-02 09:41:31 -0700

    Got some lines going on

--- js/controller.js
+++ js/controller.js
@@ -16,7 +16,30 @@ export default class Controller {
 	 * @param {CanvasRenderingContext2D} context 
 	 */
 	render(context) {
-		// TODO: Some rendering logic
+		context.globalCompositeOperation = 'multiply';
+
+		const size = 500;
+		const thickenss = 20;
+		const colors = ['#FF0', '#0FF', '#F0F'];
+		const numDirections = 3;
+		for (let i = 0; i < numDirections; i ++) {
+			const color = colors[i];
+			const numLines = 1;
+			for (let l = 0; l < numLines; l ++) {
+				const amt = l / numLines;
+
+				context.beginPath();
+				context.fillStyle = color;
+				context.moveTo(-size, -thickenss / 2);
+				context.lineTo(-size,  thickenss / 2);
+				context.lineTo( size,  thickenss / 2);
+				context.lineTo( size, -thickenss / 2);
+				context.closePath();
+				context.fill();
+			}
+
+			context.rotate(2 * Math.PI / numDirections);
+		}
 	}
 
 }
